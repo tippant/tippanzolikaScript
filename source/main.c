e5b9e0d0cb5152badf24e02e4a1a4c8a24ac6939
tippant <tamas.tippan@gmail.com>
2026-08-18 06:50:41 +0200

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <xenos/xenos.h>
+#include <console/console.h>
 
 int main(void)
 {
-    printf("TomaScript Loader test\n");
-    printf("LibXenon build successful!\n");
+    xenos_init(VIDEO_MODE_AUTO);
+    console_init();
+
+    printf("\n");
+    printf("========================================\n");
+    printf("          TomaScript Loader\n");
+    printf("========================================\n\n");
+
+    printf("TomaScript keresés...\n\n");
+
+    printf("TomaScript ellenőrzése folyamatban...\n");
+    printf("TomaScript_OK.txt ellenőrzése...\n\n");
+
+    printf("Tesztverzio: libXenon build sikeres!\n");
+    printf("\n");
+    printf("========================================\n");
 
     while (1)
     {
+        printf("\r");
     }
 
     return 0;
